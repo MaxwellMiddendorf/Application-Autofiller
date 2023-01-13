073c2e5ad5c5a4e60eb6643fc92e02bcbf6dbc5d
Max Middendorf <maxmiddendorf@Maxs-Laptop.local>
2023-01-13 11:13:47 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+
+let color = '#8F00FF';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
---
+++ popup.js
@@ -0,0 +1,113 @@
+
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id },
+            func: Main
+        }
+    );
+}
+);
+
+
+
+
+// The body of this function will be executed as a content script inside the
+// current page
+async function Main() {
+
+    console.time();
+
+    response = chrome.runtime.sendMessage({greeting: "hello"});
+    console.log(response.farewell);
+
+
+    console.log("Running...");
+    var inputLabels = inputMatcher();
+    var autofills = await autofillLoader();
+
+
+    console.log(inputLabels);
+    console.log(autofills);
+
+    autofiller();
+
+
+
+    console.timeEnd();
+    console.log("...End");
+
+
+    function autofiller() {
+        for (let [key, value] of inputLabels) {
+            value.focus();
+            if (autofills.has(key)) {
+                if (value.type === "button") { simpleButtonListHandler(value, key); } else { value.value = autofills.get(key); }
+            }
+            else if (value.type === "radio") { radioHandler(key, value); }
+            else {
+                //value.click()
+            }
+            value.style.backgroundColor = "#E2F7FF";
+        }
+    }
+
+    function radioHandler(key, value) {
+        if (["YES", "NO"].includes(key)) {
+            let radioFeildSets = Array.from(document.getElementsByTagName("fieldset")).filter(element => element.contains(value));
+            radioFeildSets.forEach(fields => {
+                Array.from(fields.getElementsByTagName("legend")).forEach(legend => { if (autofills.has(legend.textContent)) { value.click(); } });
+            }
+            )
+        }
+    }
+
+    function simpleButtonListHandler(value, key) {
+        const watcher = new MutationObserver(() => { })
+        watcher.observe(document.querySelector("body"), { subtree: true, childList: true });
+        value.click();
+        let mutes = watcher.takeRecords();
+        Array.from(mutes.pop().addedNodes[0].firstChild.parentElement.getElementsByTagName("li")).forEach(
+            li => { if (autofills.get(key).toUpperCase() === li.textContent.toUpperCase()) { li.click(); } }
+        )
+    }
+
+    function complexButtonListHandler(value, key) {
+
+    }
+
+
+    function inputMatcher() {
+        //find and categorize all inputs
+        let inputs = Array.prototype.concat(Array.from(document.getElementsByTagName("input")), Array.from(document.getElementsByTagName("button")));
+        let labels = Array.from(document.getElementsByTagName("label"));
+        let inputLabels = new Map();
+        for (let i = 0; i < inputs.length; i++) {
+            for (let j = 0; j < labels.length; j++) {
+                if (labels[j].htmlFor === inputs[i].id) {
+                    if (labels[j].textContent != null) {
+                        inputLabels.set(labels[j].textContent.replace('*', '').toUpperCase(), inputs[i]);
+                    }
+                }
+            }
+        }
+        return inputLabels;
+    }
+
+    async function autofillLoader() {
+        let res = new Map();
+        let raw = await fetch(chrome.runtime.getURL("autofills.json"))
+            .then(response => response.json())
+            .then(res => new Map(Object.entries(res)));
+        for (let [key, value] of raw) {
+            for (let ele of value) {
+                res.set(ele, key);
+            }
+        }
+        return res;
+    }
+
+}
